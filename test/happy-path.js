4f42a82c702eaa12880f804bc55632b87b974147
Oli Evans <oli@zilla.org.uk>
2015-12-17 22:14:21 +0000

    Output consistent regardless of input style

--- test/happy-path.js
+++ test/happy-path.js
@@ -1,6 +1,6 @@
 var test = require('tape')
 var fs = require('fs')
-
+var expected = require('../examples/output.dns.json')
 test('dry style', function (t) {
   var parser = require('../lib/markdown-dnsify.js')
   fs.readFile(__dirname + '/../examples/dry-style.dns.md', 'utf8', function (err, data) {
@@ -9,7 +9,7 @@ test('dry style', function (t) {
       t.error(err, 'No errors')
       t.equals(zone.origin, 'tableflip.io', 'Find origin')
       t.equals(zone.records.length, 10, 'Expands records')
-      t.deepEquals(zones, require('../examples/dry-style.dns.json'), 'Matches expected output')
+      t.deepEquals(zones, expected, 'Matches expected output')
       t.end()
     })
   })
@@ -23,7 +23,7 @@ test('zone file style', function (t) {
       t.error(err, 'No errors')
       t.equals(zone.origin, 'tableflip.io', 'Find origin')
       t.equals(zone.records.length, 10, 'Expands records')
-      t.deepEquals(zones, require('../examples/zone-file-style.dns.json'), 'Matches expected output')
+      t.deepEquals(zones, expected, 'Matches expected output')
       t.end()
     })
   })
